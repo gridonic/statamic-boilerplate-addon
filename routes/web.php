2452261698bed375b3fce5274b2130a76f2b5653
Stefan Wanzenried <stefan.wanzenried@gridonic.ch>
2023-04-26 06:37:17 +0200

    feat: add api route to create thumbnail (glide) given a preset and image asset id

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use Gridonic\StatamicBoilerplateAddon\Http\Controllers\ThumbnailFromPresetController;
 use Gridonic\StatamicBoilerplateAddon\Http\Controllers\MagicLinkController;
 use Illuminate\Support\Facades\Route;
 
 if (config('statamic.boilerplate.magic_links_enabled', false)) {
     Route::get('/portal-login/{token}', [MagicLinkController::class, 'login']);
 }
+Route::get('/api/thumbnail/{preset}/{imageAssetId}', [ThumbnailFromPresetController::class, 'thumbnail']);
---
+++ src/Http/Controllers/ThumbnailFromPresetController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Gridonic\StatamicBoilerplateAddon\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use Statamic\Facades\Antlers;
+use Statamic\Facades\Asset;
+
+class ThumbnailFromPresetController extends Controller
+{
+    public function thumbnail(string $preset, string $imageAssetId) {
+        if (!$this->isPreset($preset)) {
+            abort(404);
+        }
+
+        if (!$this->imageExists($imageAssetId)) {
+            abort(404);
+        }
+
+        $imageManipulationUrl = Antlers::parse(sprintf('{{ glide src="%s" preset="%s" }}', $imageAssetId, $preset));
+
+        return redirect($imageManipulationUrl, 301);
+    }
+
+    private function imageExists(string $imageId): bool
+    {
+        return Asset::find($imageId) !== null;
+    }
+
+    private function isPreset(string $preset): bool
+    {
+        $presets = config('statamic.assets.image_manipulation.presets', []);
+        $presetNames = array_keys($presets);
+
+        return in_array($preset, $presetNames);
+    }
+}
