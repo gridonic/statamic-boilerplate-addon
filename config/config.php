9a153b0fd40d0503ba83f08431e5ce7d274c8fcc
sandro-imhof <sandro.imhof@gridonic.ch>
2024-01-04 15:27:50 +0100

    chore: revert globals and config.php

--- config/config.php
+++ config/config.php
@@ -26,36 +26,4 @@ return [
 
     'multisite_set_data_from_origin' => env('BOILERPLATE_MULTISITE_SET_DATA_FROM_ORIGIN', true),
     'multisite_update_cached_origin_of_descendants' => env('BOILERPLATE_MULTISITE_UPDATE_CACHED_ORIGIN_OF_DESCENDANTS', true),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Theme
-    |--------------------------------------------------------------------------
-    |
-    | Optionally spice up the login and other outside-the-control-panel
-    | screens. You may choose between "rad" or "business" themes.
-    |
-    */
-
-    'theme' => env('STATAMIC_THEME', 'business'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | White Labeling
-    |--------------------------------------------------------------------------
-    |
-    | When in Pro Mode you may replace the Statamic name, logo, favicon,
-    | and add your own CSS to the control panel to match your
-    | company or client's brand.
-    |
-    */
-
-    'custom_cms_name' => env('STATAMIC_CUSTOM_CMS_NAME', 'Gridonic Boilerplate'),
-    'custom_logo_url' => [
-        'nav' => env('STATAMIC_CUSTOM_LOGO_URL_NAV', '/static/gridonic-logo.svg'),
-        'outside' => env('STATAMIC_CUSTOM_LOGO_URL_OUTSIDE', '/static/gridonic-logo-light.svg')
-    ],
-    'custom_favicon_url' => env('STATAMIC_CUSTOM_FAVICON_URL', '/favicon.ico'),
-    'custom_css_url' => env('STATAMIC_CUSTOM_CSS_URL', '/static/statamic-custom-styles.css'),
-
 ];
